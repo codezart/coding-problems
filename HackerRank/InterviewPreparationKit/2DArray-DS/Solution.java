981c3fb585f6daa70d5d8b2b11070e6f92d6ab68
Mohammed Abdur Rahman <44206264+codezart@users.noreply.github.com>
2021-02-12 16:34:16 +0300

    second commit

---
+++ HackerRank/InterviewPreparationKit/2DArray-DS/Solution.java
@@ -0,0 +1,63 @@
+/***
+ * 
+ * **/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+    
+        ArrayList<Integer> sums = new ArrayList<>();
+        int sum = 0;
+        for(int i=0; i < arr.length -2; ++i){
+            for(int j=0; j< arr[i].length -2; ++j){
+                sum += arr[0 + i][0 + j]+
+                        arr[0 + i][1 + j]+
+                        arr[0 + i][2 + j]+
+                        arr[1 + i][1 + j]+
+                        arr[2 + i][0 + j]+
+                        arr[2 + i][1 + j]+
+                        arr[2 + i][2 + j];
+                sums.add(sum);
+                sum = 0;
+            }
+        }
+
+        Collections.sort(sums);
+        return sums.get(sums.size()-1 );
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
